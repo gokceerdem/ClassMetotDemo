68ce564c4e57091ae0b2dcf96ee3665abcb7a6a0
gokceerdem <gokcerdem90@gmail.com>
2021-03-03 22:08:41 +0300

    initial commit

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ClassMetotDemo
+{
+    public class Musteri
+    {
+        public string MusteriId { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Tckn { get; set; }
+    }
+}
---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+
+                musteri[] tempArray = items;
+                items = new T[items.Length + 1];
+                //for + tab+ tab döngü oluşturur
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    items[i] = tempArray[i];
+                }
+
+                items[items.Length - 1] = item;
+
+            
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+        }
+    }
+}
