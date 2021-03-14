63c9edd8e19bb5b182810f697baff40a3c67cd09
gokceerdem <gokcerdem90@gmail.com>
2021-03-14 22:17:08 +0300

    ödev3

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -1,30 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ClassMetotDemo
 {
     public class MusteriManager
     {
-        public void MusteriEkle(Musteri musteri)
+        public void MusteriEkle(Musteri[] musteriler,Musteri musteri)
         {
+            Musteri[] tempArray = musteriler;
+            musteriler = new Musteri[musteriler.Length + 1];
+            //for + tab+ tab döngü oluşturur
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                musteriler[i] = tempArray[i];
+            }
 
-                musteri[] tempArray = items;
-                items = new T[items.Length + 1];
-                //for + tab+ tab döngü oluşturur
-                for (int i = 0; i < tempArray.Length; i++)
-                {
-                    items[i] = tempArray[i];
-                }
+            musteriler[musteriler.Length - 1] = musteri;
 
-                items[items.Length - 1] = item;
-
-            
         }
 
-        public void MusteriSil(Musteri musteri)
+        public void MusteriSil(Musteri[] musteriler,string musteriId)
         {
+
+
+            Musteri  silMusteri = null;
+
+            foreach (var p in musteriler) {
+                if(musteriId == p.MusteriId)
+                {
+                    silMusteri = p;
+                }
+            }
+
+
+            musteriler = musteriler.Except(new Musteri[] { silMusteri }).ToArray();
+            foreach (var i in musteriler)
+
+                Console.WriteLine(i.Ad + " " + i.Soyad + "  MÜŞTERİ NO: " + i.MusteriId + "  TC NO: " + i.Tckn);
+
+
         }
 
         public void MusteriListele(Musteri[] musteriler)
         {
-        }
+            foreach (var i in musteriler)
+                
+                Console.WriteLine(i.Ad + " " + i.Soyad + "  MÜŞTERİ NO: " + i.MusteriId + "  TC NO: " + i.Tckn);
+
+            }
     }
+
+
+
+
+
 }
+
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace ClassMetotDemo
 {
@@ -6,7 +7,48 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            Musteri mus1 = new Musteri();
+            Musteri mus2 = new Musteri();
+            Musteri mus3 = new Musteri();
+            mus1.MusteriId = "1";
+            mus1.Ad = "Ali";
+            mus1.Soyad = "Cebeci";
+            mus1.Tckn = "45934992018";
+
+            mus2.MusteriId = "2";
+            mus2.Ad = "Alixx";
+            mus2.Soyad = "Cebecixx";
+            mus2.Tckn = "56934992019";
+
+            mus3.MusteriId = "3";
+            mus3.Ad = "Can";
+            mus3.Soyad = "Kurt";
+            mus3.Tckn = "38274402938";
+
+
+            Musteri[] musteris = new Musteri[] {mus1,mus2, mus3};
+            /* foreach (Musteri musteri in musteris)
+             {
+                 Console.WriteLine("AD: " + musteri.Ad);
+                 Console.WriteLine("SOYAD: " + musteri.Soyad);
+                 Console.WriteLine("Tckn: " + musteri.Tckn);
+                 Console.WriteLine("musno: " + musteri.MusteriId);
+
+
+             }
+            */
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriListele(musteris);
+
+
+            Console.WriteLine("-----------------------");
+
+            musteriManager.MusteriSil(musteris, "3");
+
         }
     }
 }
